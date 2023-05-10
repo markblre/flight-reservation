b27e247e9e9de4e254af71588f71a0dbf5d43fec
Mark Ballereau <mark.b@live.fr>
2023-05-10 18:50:27 +0200

    Ajout de commentaires

--- src/main/java/aeroport/Aeroport.java
+++ src/main/java/aeroport/Aeroport.java
@@ -2,14 +2,33 @@ package aeroport;
 
 import java.util.Map;
 
+/**
+ * Classe representant un aeroport
+ */
 public class Aeroport {
 
+    /**
+     * Le nom de l'aeroport
+     */
     private String nom;
 
+    /**
+     * La ville de l'aeroport
+     */
     private final Ville ville;
 
+    /**
+     * La liste des villes desservies par l'aeroport avec le nombre de vol
+     */
     private final Map<Ville, Integer> villesDesservies = new java.util.HashMap<>(); // Integer = nombre de vol vers la ville
 
+    /**
+     * Constructeur de la classe Aeroport
+     *
+     * @param nom le nom de l'aeroport
+     * @param ville la ville de l'aeroport
+     * @exception IllegalArgumentException si le nom ou la ville est null
+     */
     public Aeroport(String nom, Ville ville) {
         if (nom == null || ville == null) {
             throw new IllegalArgumentException("nom and ville cannot be null");
@@ -21,22 +40,49 @@ public class Aeroport {
         ville.addAeroportWithoutBidirectional(this);
     }
 
+    /**
+     * Retourne le nom de l'aeroport
+     *
+     * @return le nom de l'aeroport
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Modifie le nom de l'aeroport
+     *
+     * @param nom le nouveau nom de l'aeroport
+     * @exception IllegalArgumentException si le nom est null
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * Retourne la ville de l'aeroport
+     *
+     * @return la ville de l'aeroport
+     */
     public Ville getVille() {
         return ville;
     }
 
+    /**
+     * Retourne la liste des villes desservies par l'aeroport
+     *
+     * @return la liste des villes desservies par l'aeroport
+     */
     public Map<Ville, Integer> getVillesDesservies() {
         return villesDesservies;
     }
 
+    /**
+     * Comptabilise un vol de plus de l'aeroport vers la ville
+     *
+     * @param ville la ville d'arrivée du vol
+     * @exception IllegalArgumentException si la ville est null
+     */
     protected void addVolVers(Ville ville) {
         if (this.villesDesservies.containsKey(ville)) {
             this.villesDesservies.put(ville, villesDesservies.get(ville) + 1);
@@ -46,6 +92,12 @@ public class Aeroport {
         ville.addVolDepuisWithoutBidirectional(this);
     }
 
+    /**
+     * Comptabilise un vol de moins de l'aeroport vers la ville
+     *
+     * @param ville la ville d'arrivée du vol
+     * @exception IllegalArgumentException si la ville est null
+     */
     protected void removeVolVers(Ville ville) {
         if (this.villesDesservies.containsKey(ville)) {
             if (this.villesDesservies.get(ville) == 1) {
--- src/main/java/aeroport/Ville.java
+++ src/main/java/aeroport/Ville.java
@@ -4,13 +4,31 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
+/**
+ * Classe representant une ville
+ */
 public class Ville {
+    /**
+     * Le nom de la ville
+     */
     private String nom;
 
+    /**
+     * La liste des aeroports de la ville
+     */
     private final Collection<Aeroport> aeroports = new ArrayList<>();
 
+    /**
+     * La liste des aeroports qui desservent la ville avec le nombre de vol
+     */
     private final Map<Aeroport, Integer> aeroportsDesservant = new java.util.HashMap<>(); // Integer = nombre de vol provenant de l'aeroport
 
+    /**
+     * Constructeur de la classe Ville
+     *
+     * @param nom le nom de la ville
+     * @exception IllegalArgumentException si le nom est null
+     */
     public Ville(String nom) {
         if (nom == null) {
             throw new IllegalArgumentException("nom cannot be null");
@@ -19,34 +37,74 @@ public class Ville {
         this.nom = nom;
     }
 
+    /**
+     * Retourne le nom de la ville
+     *
+     * @return le nom de la ville
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Modifie le nom de la ville
+     *
+     * @param nom le nouveau nom de la ville
+     * @exception IllegalArgumentException si le nom est null
+     */
     public void setNom(String nom) {
+        if(nom == null){
+            throw new IllegalArgumentException("nom cannot be null");
+        }
         this.nom = nom;
     }
 
+    /**
+     * Retourne la liste des aeroports de la ville
+     *
+     * @return la liste des aeroports de la ville
+     */
     public Collection<Aeroport> getAeroports() {
         return aeroports;
     }
 
+    /**
+     * Ajoute un aeroport à la liste des aeroports de la ville
+     *
+     * @param aeroport l'aeroport à ajouter
+     */
+    protected void addAeroportWithoutBidirectional(Aeroport aeroport) {
+        this.aeroports.add(aeroport);
+    }
+
+    /**
+     * Supprime l'aeroport de la ville
+     *
+     * @param aeroport l'aeroport à ajouter
+     * @exception IllegalArgumentException si l'aeroport est toujours desservi par des vols
+     */
     public void removeAeroport(Aeroport aeroport) {
         if (!aeroport.getVillesDesservies().isEmpty()) {
             throw new IllegalStateException("L'aeroport est toujours desservi par des vols.");
         }
         this.aeroports.remove(aeroport);
-        // inutile de modifier l'attribut ville de l'aeroport car il n'existe plus
+        // inutile de modifier l'attribut ville de l'aeroport car un aeroport ne peut pas changer de ville (objet "abandonné")
     }
 
-    protected void addAeroportWithoutBidirectional(Aeroport aeroport) {
-        this.aeroports.add(aeroport);
-    }
-
+    /**
+     * Retourne la liste des aeroports qui desservent la ville avec le nombre de vol
+     *
+     * @return une map qui contient les aeroports qui desservent la ville avec le nombre de vol
+     */
     public Map<Aeroport, Integer> getAeroportsDesservant() {
         return aeroportsDesservant;
     }
 
+    /**
+     * Comptabilise un vol de plus de l'eroport vers la ville
+     *
+     * @param aeroport l'aeroport depuis lequel le vol est ajoute
+     */
     protected void addVolDepuisWithoutBidirectional(Aeroport aeroport) {
         if (this.aeroportsDesservant.containsKey(aeroport)) {
             this.aeroportsDesservant.put(aeroport, this.aeroportsDesservant.get(aeroport) + 1);
@@ -55,6 +113,11 @@ public class Ville {
         }
     }
 
+    /**
+     * Comptabilise un vol de moins de l'aeroport vers la ville
+     *
+     * @param aeroport l'aeroport depuis lequel le vol est supprime
+     */
     protected void removeVolDepuisWithoutBidirectional(Aeroport aeroport) {
         if (this.aeroportsDesservant.containsKey(aeroport)) {
             if (this.aeroportsDesservant.get(aeroport) == 1) {
